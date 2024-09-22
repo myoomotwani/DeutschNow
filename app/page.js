d0e06122521f63bb0209074a19520a8273e375a7
Vyas Giri <vyasgiri25@gmail.com>
2024-09-23 02:59:39 +0530

    Added all three scenarios and generating audio at runtime

--- app/page.js
+++ app/page.js
@@ -1,53 +1,23 @@
 "use client";
+import { checkDialogues1, checkDialogues2, checkDialogues3, dialogues1, dialogues2, dialogues3 } from "@/utils/dialogues";
+import levenshtein from "js-levenshtein";
 import Image from "next/image";
 import { SnackbarProvider, enqueueSnackbar } from "notistack";
 import React, {useEffect, useState, useRef} from "react";
 
 
-const dialogues = [
-  ["You", "Entschuldigung, wo kann ich Tickets kaufen?", "/audios/audio1.mp3", "Excuse me, where can I buy tickets?"],
-  ["Seller", "Die Ticketschalter befinden sich auf der rechten Seite.", "/audios/audio2.mp3", "The ticket counters are on the right side."],
-  ["Seller", "Möchten Sie eine Fahrkarte für die Stadt oder ein Fernreiseticket?", "/audios/audio3.mp3", "Would you like a city ticket or a long-distance ticket?"],
-  ["You", "Ich brauche ein Ticket nach München.", "/audios/audio4.mp3", "I need a ticket to Munich."],
-  ["You", "Wie viel kostet es?", "/audios/audio5.mp3", "How much does it cost?"],
-  ["Seller", "Eine einfache Fahrt kostet 20 Euro und ein Hin- und Rückfahrtticket kostet 35 Euro.", "/audios/audio6.mp3", "A one-way ticket costs 20 euros, and a round-trip ticket costs 35 euros."],
-  ["Seller", "Welche Art von Ticket möchten Sie?", "/audios/audio7.mp3", "Which type of ticket would you like?"],
-  ["You", "Ich nehme ein einfaches Ticket, bitte.", "/audios/audio8.mp3", "I will take a one-way ticket, please."],
-  ["You", "Wie lange ist das Ticket gültig?", "/audios/audio9.mp3", "How long is the ticket valid?"],
-  ["Seller", "Das Ticket ist für den ganzen Tag gültig.", "/audios/audio10.mp3", "The ticket is valid for the entire day."],
-  ["Seller", "Möchten Sie bar oder mit Karte bezahlen?", "/audios/audio11.mp3", "Would you like to pay in cash or by card?"],
-  ["You", "Mit Karte, bitte.", "/audios/audio12.mp3", "By card, please."],
-  ["You", "Vielen Dank!", "/audios/audio13.mp3", "Thank you very much!"],
-  ["Seller", "Gerne! Hier ist Ihr Ticket. Gute Reise!", "/audios/audio14.mp3", "You're welcome! Here is your ticket. Have a good trip!"]
-];
-
-
-const checkDialogues = [
-  ["You", "Entschuldigung wo kann ich Tickets kaufen"],
-  ["Seller", "Die Ticketschalter befinden sich auf der rechten Seite"],
-  ["Seller", "Möchten Sie eine Fahrkarte für die Stadt oder ein Fernreiseticket"],
-  ["You", "ich brauche ein Ticket nach München"],
-  ["You", "wie viel kostet es"],
-  ["Seller", "Eine einfache Fahrt kostet 20 Euro und ein Hin und Rückfahrtticket kostet 35 Euro"],
-  ["Seller", "Welche Art von Ticket möchten Sie"],
-  ["You", "ich nehme ein einfaches Ticket bitte"],
-  ["You", "wie lange ist das Ticket gültig"],
-  ["Seller", "Das Ticket ist für den ganzen Tag gültig"],
-  ["Seller", "Möchten Sie bar oder mit Karte bezahlen"],
-  ["You", "mit Karte bitte"],
-  ["You", "vielen Dank"],
-  ["Seller", "gerne Hier ist Ihr Ticket Gute Reise"]
-];
-
-
 export default function Home() {
+  const [dialogues, setDialogues] = useState(dialogues1);
+  const [checkDialogues, setCheckDialogues] = useState(checkDialogues1);
   const [currentDialogueIndex, setCurrentDialogueIndex] = useState(-1);
-    const [userSpeech, setUserSpeech] = useState("");
-    const [isCorrect, setIsCorrect] = useState(false);
-    const lastTextRef = useRef(null);
-    const [isClient, setIsClient] = useState(false);
-    const [allowContinue, setAllowContinue] = useState(false);
-    const scrollableSectionRef = useRef(null);
+  const [userSpeech, setUserSpeech] = useState("");
+  const [isCorrect, setIsCorrect] = useState(false);
+  const lastTextRef = useRef(null);
+  const [isClient, setIsClient] = useState(false);
+  const [allowContinue, setAllowContinue] = useState(false);
+  const scrollableSectionRef = useRef(null);
+  const [speaking, setSpeaking] = useState(false);
+  const threshold = 7;
 
     useEffect(() => {
       // This ensures the code runs only on the client side
@@ -60,11 +30,64 @@ export default function Home() {
           lastTextRef.current.scrollIntoView({ behavior: 'smooth' });
       }
   }, [currentDialogueIndex]);
-    // const scrollToElement = () => {
-    //   if (lastTextRef.current) {
-    //     lastTextRef.current.scrollIntoView({ behavior: 'smooth' });
-    //   }
-    // };
+
+  const handleScenarioChange = (e) => {
+    const selectedScenario = e.target.value;
+    switch (selectedScenario) {
+        case '1':
+            setDialogues(dialogues1);
+            setCheckDialogues(checkDialogues1);
+            break;
+        case '2':
+            setDialogues(dialogues2);
+            setCheckDialogues(checkDialogues2);
+            break;
+        case '3':
+            setDialogues(dialogues3);
+            setCheckDialogues(checkDialogues3);
+            break;
+        default:
+            setDialogues(dialogues1);
+            setCheckDialogues(checkDialogues1);
+    }
+    if (currentDialogueIndex !== 0) {
+      setCurrentDialogueIndex(-1); // Reset the dialogue index when scenario changes
+    }
+};
+
+  const handleSpeak = (text) => {
+    if (!text) {
+      alert('Please enter some text');
+      return;
+    }
+    console.log(currentDialogueIndex)
+
+    // Create a new SpeechSynthesisUtterance instance
+    const utterance = new SpeechSynthesisUtterance(text);
+
+    // Set language to German (de-DE)
+    utterance.lang = 'de-DE';
+
+    // Set up event listeners to monitor speech progress
+    utterance.onstart = () => {
+      setSpeaking(true);
+      console.log('Speech started');
+    };
+
+    utterance.onend = () => {
+      setSpeaking(false);
+      console.log('Speech finished');
+    };
+
+    utterance.onerror = (event) => {
+      console.error('Speech synthesis error:', event.error);
+      setSpeaking(false);
+    };
+
+    // Speak the text
+    window.speechSynthesis.speak(utterance);
+  };
+
 
     const startRecognition = () => {
       if (!isClient) return;
@@ -116,7 +139,13 @@ export default function Home() {
     };
 
     const checkDialogue = (speechResult, expectedDialogue) => {
-        return speechResult.trim() === expectedDialogue.trim();
+        const distance = levenshtein(speechResult, expectedDialogue);
+
+    if (distance <= threshold) {
+        return true; // Accept the response
+    } else {
+        return false; // Ask the user to try again
+    }
     };
 
     // const startRecognition = () => {
@@ -130,7 +159,8 @@ export default function Home() {
           setIsCorrect(false);
           setUserSpeech("");
           setAllowContinue(false);
-          playAudio(dialogues[currentDialogueIndex + 1][2])
+          handleSpeak(dialogues[currentDialogueIndex + 1][1])
+          // playAudio(dialogues[currentDialogueIndex + 1][2])
           // scrollToElement();
       }
   };
@@ -140,11 +170,17 @@ export default function Home() {
       <SnackbarProvider dense />
       <div className="w-full flex flex-col space-y-1 lg:space-y-2 xl:space-y-3 items-center justify-center">
         <span className="text-[#036A8C] font-extrabold text-lg md:text-xl xl:text-2xl">DeutschNow</span>
-        <div className="bg-[url('/banner1.png')] h-56 sm:h-60 w-screen bg-bottom md:bg-center bg-cover bg-no-repeat bg-fixed"></div>
-        <div className="w-[90%] sm:w-[80%] md:w-[70%] flex flex-col items-center justify-center shadow-[#00000017] shadow-lg drop-shadow-lg rounded-lg">
+        <div className="bg-[url('/banner1.png')] h-56 !mb-2 sm:h-60 w-screen bg-bottom md:bg-center bg-cover bg-no-repeat bg-fixed"></div>
+        <select onChange={handleScenarioChange} className="p-2 border border-blue-700 rounded-xl outline-0 ring-0">
+          <option value="1">Scenario 1: Buying a Ticket</option>
+          <option value="2">Scenario 2: Asking for Directions</option>
+          <option value="3">Scenario 3: Finding a Restaurant</option>
+        </select>
+        <div className="w-[90%] !mt-2 sm:w-[80%] md:w-[70%] flex flex-col items-center justify-center shadow-[#00000017] shadow-lg drop-shadow-lg rounded-lg">
           <div className="bg-[#036A8C] text-white text-xs sm:text-base rounded-t-lg px-3 py-1 xl:p-3 w-full lg:text-lg z-10">
             Conversation at bus stand
           </div>
+          {/* <button onClick={handleSpeak}>Speak</button> */}
           <div className="overflow-y-scroll w-full space-y-3 rounded-2xl p-3 h-60 sm:h-52 md:h-48 xl:h-60" ref={scrollableSectionRef}>
             {currentDialogueIndex < 0 && <div className="flex flex-col space-y-2 items-center justify-center w-full h-full">
               <button onClick={proceedToNext} className="bg-green-500 rounded-3xl py-2 px-4 text-white">Start</button>
@@ -169,8 +205,8 @@ export default function Home() {
                             />
                             <div className={`flex flex-col items-center justify-center space-y-1 rounded-lg p-1 sm:px-3 ${dialogue[0] === "Seller" && "bg-[#efefef]"} ${dialogue[0] === "You" && "bg-[#036A8C1A]"} ${index === currentDialogueIndex && dialogue[0] === "You" && userSpeech && !isCorrect && "bg-[#FEDFDF]"}`}>
                               <div className="flex items-center justify-center space-x-2">
-                                <button onClick={() => {
-                                  playAudio(dialogue[2])
+                                <button disabled={speaking} onClick={() => {
+                                  handleSpeak(dialogue[1])
                                 }}>
                                   <Image 
                                       src="/speakButton.png" 
@@ -182,8 +218,8 @@ export default function Home() {
                                 </button>
                                 <span className="text-xs sm:text-base lg:text-lg">{dialogue[1]}</span>
                               </div>
-                              <span className="text-[10px] sm:text-sm">{dialogue[3]}</span>
-                              {currentDialogueIndex < 17 && dialogue[0] === "You" && index === currentDialogueIndex && (
+                              <span className="text-[10px] sm:text-sm">{dialogue[2]}</span>
+                              {currentDialogueIndex < dialogues.length && dialogue[0] === "You" && index === currentDialogueIndex && (
                 <div className="user-input flex items-center justify-center flex-col space-y-1">
                   {userSpeech !== "" && isCorrect ? null : <button onClick={startRecognition} className="rounded-3xl p-2 px-4 bg-[#036A8C] text-white flex items-center justify-center space-x-2 text-xs">
                       <Image src={"/microphone.png"} alt="speak" width={13} height={12} />
@@ -204,9 +240,9 @@ export default function Home() {
                 <div ref={lastTextRef} />  
             </div>
 
-            {currentDialogueIndex > 14 && isCorrect && <span className="px-3 py-1 sm:py-2 text-green-500 text-center text-[10px] sm:text-xs md:text-sm border-t border-green-500">Success!!!<br/>You&apos;ve practiced your speaking skills at a bus stand. You&apos;re now better equipped to handle the ticket booking process in Hindi.</span>}
+            {currentDialogueIndex > dialogues.length - 1 && isCorrect && <span className="px-3 py-1 sm:py-2 text-green-500 text-center text-[10px] sm:text-xs md:text-sm border-t border-green-500">Success!!!<br/>You&apos;ve practiced your speaking skills at a bus stand. You&apos;re now better equipped to handle the ticket booking process in Hindi.</span>}
             
-          {currentDialogueIndex > -1 && currentDialogueIndex < 15 && 
+          {currentDialogueIndex > -1 && currentDialogueIndex < dialogues.length - 1 && 
           <div className="border-t border-[#D9D9D9] w-full flex items-center justify-center z-10 py-2">
             {/* <button>Continue</button> */}
             {/* dialogues[currentDialogueIndex][0] === "You" &&  */}
---
+++ utils/dialogues.js
@@ -0,0 +1,85 @@
+export const dialogues1 = [
+    ["You", "Entschuldigung, wo kann ich Tickets kaufen?", "Excuse me, where can I buy tickets?"],
+    ["Seller", "Die Ticketschalter befinden sich auf der rechten Seite.", "The ticket counters are on the right side."],
+    ["Seller", "Möchten Sie eine Fahrkarte für die Stadt oder ein Fernreiseticket?", "Would you like a city ticket or a long-distance ticket?"],
+    ["You", "Ich brauche ein Ticket nach München.", "I need a ticket to Munich."],
+    ["You", "Wie viel kostet es?", "How much does it cost?"],
+    ["Seller", "Eine einfache Fahrt kostet 20 Euro und ein Hin- und Rückfahrtticket kostet 35 Euro.","A one-way ticket costs 20 euros, and a round-trip ticket costs 35 euros."],
+    ["Seller", "Welche Art von Ticket möchten Sie?","Which type of ticket would you like?"],
+    ["You", "Ich nehme ein einfaches Ticket, bitte.","I will take a one-way ticket, please."],
+    ["You", "Wie lange ist das Ticket gültig?","How long is the ticket valid?"],
+    ["Seller", "Das Ticket ist für den ganzen Tag gültig.", "The ticket is valid for the entire day."],
+    ["Seller", "Möchten Sie bar oder mit Karte bezahlen?", "Would you like to pay in cash or by card?"],
+    ["You", "Mit Karte, bitte.", "By card, please."],
+    ["You", "Vielen Dank!", "Thank you very much!"],
+    ["Seller", "Gerne! Hier ist Ihr Ticket. Gute Reise!", "You're welcome! Here is your ticket. Have a good trip!"]
+];
+
+export const checkDialogues1 = [
+    ["You", "Entschuldigung wo kann ich Tickets kaufen"],
+    ["Seller", "Die Ticketschalter befinden sich auf der rechten Seite"],
+    ["Seller", "Möchten Sie eine Fahrkarte für die Stadt oder ein Fernreiseticket"],
+    ["You", "ich brauche ein Ticket nach München"],
+    ["You", "wie viel kostet es"],
+    ["Seller", "Eine einfache Fahrt kostet 20 Euro und ein Hin und Rückfahrtticket kostet 35 Euro"],
+    ["Seller", "Welche Art von Ticket möchten Sie"],
+    ["You", "ich nehme ein einfaches Ticket bitte"],
+    ["You", "wie lange ist das Ticket gültig"],
+    ["Seller", "Das Ticket ist für den ganzen Tag gültig"],
+    ["Seller", "Möchten Sie bar oder mit Karte bezahlen"],
+    ["You", "mit Karte bitte"],
+    ["You", "vielen Dank"],
+    ["Seller", "gerne Hier ist Ihr Ticket Gute Reise"]
+];
+
+
+export const dialogues2 =  [
+    ["Seller", "Guten Tag! Ihren Reisepass, bitte.", "Good day! Your passport, please."],
+    ["You", "Guten Tag! Hier ist mein Reisepass.", "Good day! Here is my passport."],
+    ["Seller", "Was ist der Zweck Ihrer Reise nach Deutschland?", "What is the purpose of your trip to Germany?"],
+    ["You", "Ich bin hier für Urlaub / geschäftlich.", "I am here for vacation / business."],
+    ["Seller", "Wie lange werden Sie bleiben?", "How long will you be staying?"],
+    ["You", "Ich werde zwei Wochen bleiben.", "I will be staying for two weeks."],
+    ["Seller", "Wo werden Sie wohnen?", "Where will you be staying?"],
+    ["You", "Ich werde in einem Hotel in Berlin wohnen.", "I will be staying at a hotel in Berlin."],
+    ["Seller", "Haben Sie etwas zu verzollen?", "Do you have anything to declare?"],
+    ["You", "Nein, ich habe nichts zu verzollen.", "No, I have nothing to declare."],
+    ["Seller", "Vielen Dank. Willkommen in Deutschland!", "Thank you. Welcome to Germany!"],
+    ["You", "Danke schön!", "Thank you very much!"]
+];
+
+export const checkDialogues2 = [
+    ["Seller", "Guten Tag Ihren Reisepass bitte"],
+    ["You", "Guten Tag Hier ist mein Reisepass"],
+    ["Seller", "Was ist der Zweck Ihrer Reise nach Deutschland"],
+    ["You", "ich bin hier für Urlaub geschäftlich"],
+    ["Seller", "Wie lange werden Sie bleiben"],
+    ["You", "ich werde zwei Wochen bleiben"],
+    ["Seller", "Wo werden Sie wohnen"],
+    ["You", "ich werde in einem Hotel in Berlin wohnen"],
+    ["Seller", "Haben Sie etwas zu verzollen"],
+    ["You", "nein ich habe nichts zu verzollen"],
+    ["Seller", "Vielen Dank Willkommen in Deutschland"],
+    ["You", "danke schön"]
+];
+
+
+export const dialogues3 = [
+    ["Seller", "Entschuldigung, suchen Sie Ihr Gepäck?", "Excuse me, are you looking for your luggage?"],
+    ["You", "Ja, mein Koffer ist nicht auf dem Band.", "Yes, my suitcase is not on the belt."],
+    ["Seller", "Haben Sie die Gepäcknummer?", "Do you have the baggage claim number?"],
+    ["You", "Ja, hier ist sie.", "Yes, here it is."],
+    ["Seller", "Wir werden Ihr Gepäck suchen. Es kann ein paar Minuten dauern.", "We will search for your luggage. It may take a few minutes."],
+    ["You", "Vielen Dank für Ihre Hilfe!", "Thank you for your help!"],
+    ["Seller", "Gern geschehen. Ich hoffe, Sie finden es bald.", "You're welcome. I hope you find it soon."]
+];
+
+export const checkDialogues3 = [
+    ["Seller", "Entschuldigung suchen Sie Ihr Gepäck"],
+    ["You", "ja mein Koffer ist nicht auf dem Band"],
+    ["Seller", "Haben Sie die Gepäcknummer"],
+    ["You", "ja hier ist sie"],
+    ["Seller", "wir werden Ihr Gepäck suchen Es kann ein paar Minuten dauern"],
+    ["You", "vielen Dank für Ihre Hilfe"],
+    ["Seller", "gern geschehen Ich hoffe Sie finden es bald"]
+];
