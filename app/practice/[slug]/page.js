abeb71ae18d3587e742ce0a473b9c1a88038f384
Vyas Giri <vyasgiri25@gmail.com>
2024-09-25 12:50:35 +0530

    changed banner images corrected headings

--- app/practice/[slug]/page.js
+++ app/practice/[slug]/page.js
@@ -7,7 +7,7 @@ import React, {useEffect, useState, useRef} from "react";
 
 
 export default function Page({ params }) {
-  const [dialogues, setDialogues] = useState(dialogues1);
+  const [dialogues, setDialogues] = useState([]);
   const [checkDialogues, setCheckDialogues] = useState(checkDialogues1);
   const [currentDialogueIndex, setCurrentDialogueIndex] = useState(-1);
   const [userSpeech, setUserSpeech] = useState("");
@@ -17,17 +17,20 @@ export default function Page({ params }) {
   const [allowContinue, setAllowContinue] = useState(false);
   const scrollableSectionRef = useRef(null);
   const [speaking, setSpeaking] = useState(false);
-  const threshold = 7;
+  const threshold = 3;
   const slug = params.slug
   useEffect(() => {
     if (slug === "scenario1") {
       setDialogues(dialogues1)
+      setCheckDialogues(checkDialogues1)
     }
     else if (slug === "scenario2") {
       setDialogues(dialogues2)
+      setCheckDialogues(checkDialogues2)
     }
     else if (slug === "scenario3") {
       setDialogues(dialogues3)
+      setCheckDialogues(checkDialogues3)
     }
   }, [slug])
 
@@ -43,36 +46,11 @@ export default function Page({ params }) {
       }
   }, [currentDialogueIndex]);
 
-  const handleScenarioChange = (e) => {
-    const selectedScenario = e.target.value;
-    switch (selectedScenario) {
-        case '1':
-            setDialogues(dialogues1);
-            setCheckDialogues(checkDialogues1);
-            break;
-        case '2':
-            setDialogues(dialogues2);
-            setCheckDialogues(checkDialogues2);
-            break;
-        case '3':
-            setDialogues(dialogues3);
-            setCheckDialogues(checkDialogues3);
-            break;
-        default:
-            setDialogues(dialogues1);
-            setCheckDialogues(checkDialogues1);
-    }
-    if (currentDialogueIndex !== -1) {
-      setCurrentDialogueIndex(-1); // Reset the dialogue index when scenario changes
-    }
-};
-
   const handleSpeak = (text) => {
     if (!text) {
       alert('Please enter some text');
       return;
     }
-    console.log(currentDialogueIndex)
 
     // Create a new SpeechSynthesisUtterance instance
     const utterance = new SpeechSynthesisUtterance(text);
@@ -182,18 +160,19 @@ export default function Page({ params }) {
       <SnackbarProvider dense />
       <div className="w-full flex flex-col space-y-1 lg:space-y-2 xl:space-y-3 items-center justify-center">
         <span className="text-[#036A8C] font-extrabold text-lg md:text-xl xl:text-2xl">DeutschNow</span>
-        <div className="bg-[url('/banner1.png')] h-56 !mb-2 sm:h-60 w-screen bg-bottom md:bg-center bg-cover bg-no-repeat bg-fixed"></div>
+        <div className={`${slug === "scenario1" && "bg-[url('/banner1.jpeg')]"} ${slug === "scenario2" && "bg-[url('/banner2.jpeg')]"} ${slug === "scenario3" && "bg-[url('/banner3.jpeg')]"} h-56 !mb-2 sm:h-64 lg:h-72 w-screen bg-bottom md:bg-center bg-cover bg-no-repeat bg-fixed`}></div>
         <div className="w-[90%] !mt-2 sm:w-[80%] md:w-[70%] flex flex-col items-center justify-center shadow-[#00000017] shadow-lg drop-shadow-lg rounded-lg">
           <div className="bg-[#036A8C] text-white text-xs sm:text-base rounded-t-lg px-3 py-1 xl:p-3 w-full lg:text-lg z-10">
-            {slug === "scenario1" && <span>Buying a Ticket.</span>}
-            {slug === "scenario2" && <span>Asking for Directions.</span>}
-            {slug === "scenario3" && <span>Finding a Restaurant.</span>}
+            {slug === "scenario1" && <span>Buying a Train Ticket in Germany</span>}
+            {slug === "scenario2" && <span>Going Through Immigration in Germany</span>}
+            {slug === "scenario3" && <span>Handling Lost Luggage at the Airport</span>}
           </div>
           {/* <button onClick={handleSpeak}>Speak</button> */}
           <div className="overflow-y-scroll w-full space-y-3 rounded-2xl p-3 h-60 sm:h-52 md:h-48 xl:h-60" ref={scrollableSectionRef}>
             {currentDialogueIndex < 0 && <div className="flex flex-col space-y-2 items-center justify-center w-full h-full">
               <button onClick={proceedToNext} className="bg-green-500 rounded-3xl py-2 px-4 text-white">Start</button>
-              <span className="max-w-[80%] text-center">Practice speaking German with real life scenarios. Speak your lines to keep the conversation going.
+              <span className="max-w-[80%] text-center">
+                Practice speaking German with real life scenarios. Speak your lines to keep the conversation going.
               </span>
             </div>}
                 {dialogues.slice(0, currentDialogueIndex + 1).map((dialogue, index) => (
@@ -249,7 +228,7 @@ export default function Page({ params }) {
                 <div ref={lastTextRef} />  
             </div>
 
-            {currentDialogueIndex > dialogues.length - 1 && isCorrect && <span className="px-3 py-1 sm:py-2 text-green-500 text-center text-[10px] sm:text-xs md:text-sm border-t border-green-500">Success!!!<br/>You&apos;ve Paged your speaking skills at a bus stand. You&apos;re now better equipped to handle the ticket booking process in Hindi.</span>}
+            {currentDialogueIndex > dialogues.length - 2 && <span className="px-3 py-1 sm:py-2 text-green-500 text-center text-[10px] sm:text-xs md:text-sm border-t border-green-500">Success!!!<br/>You&apos;ve practiced your speaking skills at {slug === "scenario1" && "a metro/railway station."} {slug === "scenario2" && "an airport."} {slug === "scenario3" && "an airport."} You&apos;re now better equipped to handle {slug === "scenario1" && "enquiring for train tickets in german."} {slug === "scenario2" && "immigration process in german."} {slug === "scenario3" && "lost luggage search at an airport in german."}</span>}
             
           {currentDialogueIndex > -1 && currentDialogueIndex < dialogues.length - 1 && 
           <div className="border-t border-[#D9D9D9] w-full flex items-center justify-center z-10 py-2">
